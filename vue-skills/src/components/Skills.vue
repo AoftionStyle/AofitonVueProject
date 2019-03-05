badce1559c72e0667502b47dd1a57aa3827860cb
aof <chutipon.ppn@gmail.com>
2019-03-05 15:12:31 +0700

    v-bind style class

--- vue-skills/src/components/Skills.vue
+++ vue-skills/src/components/Skills.vue
@@ -5,6 +5,8 @@
     {{ btnState ? 'button is disabled' : 'button is active' }}
     <button v-on:click="" v-bind:disabled="btnState"> ChangeName</button>
 
+    <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+
     <div class="holder">
       <ul>
         <li v-for="(data, index) in skills" :key='index'>{{index}}.{{data.skill}}</li>
@@ -14,7 +16,8 @@
       <p v-else> You habe less than or equal to 1 skill</p>
 
       <div v-bind:class="{ alert: showAlert, 'another-class': showClass }"></div>
-      <!-- <div v-bind:class="alertObject"></div> -->
+      <div v-bind:class="alertObject"></div>
+      <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
     </div>
   </div>
 </template>
@@ -26,15 +29,19 @@ export default {
     return {
       name: "Aoftion",
       btnState: true,
+      skill: '',
       skills: [
         {"skill" : "Vue.js"},
         {"skill" : "Frontend Developer"}
       ],
       showAlert: true,
       showClass: true,
-      // alertObjet: {
-      //   //alert: true
-      // }
+      alertObject: {
+        alert: true
+      },
+      bgColor: "blue",
+      bgWidth: "100%",
+      bgHeight: '30px'
     }
   }
 }
@@ -64,6 +71,8 @@ a {
 .another-class {
   border: 5px solid black;
 }
+
+
 </style>
 
 <style src="./Skills.css" scoped>
