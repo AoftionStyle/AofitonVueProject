8094a658723547bed2451659e3fd3d55fed3662d
aof <chutipon.ppn@gmail.com>
2019-03-06 11:05:15 +0700

    change background

--- vue-skills/src/components/Skills.vue
+++ vue-skills/src/components/Skills.vue
@@ -74,15 +74,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-a {
-  color: #42b983;
+.holder {
+  background: #fff;
 }
 
-</style>
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
 
-<style src="./Skills.css" scoped>
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #E0EDF4;
+  border-left: 5px solid #3EB3F6;
+  margin-bottom: 2px;
+  color: #3E5252;
+}
 
+P {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
 </style>
